03eb8587d47a3a59b9730a2f6e161ed7d68bd94f
whatsappfreecalling <flarewebteam@gmail.com>
2015-04-05 16:34:13 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,76 @@
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+   
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Whatsapp Video Calling</title>
+<meta property="og:title" content="WhatsApp Video Calling v 1.0">
+
+    <meta property="og:type" content="website">
+
+    <meta name="robots" content="noindex, nofollow">
+
+    <meta property="og:image" content="http://i.imgur.com/9s7vUet.jpg?1">
+
+    <meta property="og:description" content="Click on the Picture to activate WhatsApp Video Calling!! Hey, i am inviting you to activate WhatsApp VIDEO Calling (v 1.0) click here to activate now --> http://WhatsappCalling.co"/> 
+
+<style>h2{ text-align:center; border:1px solid #7fd4e9; color:#369; font-size:110%; padding:5px; 
+background:#7fd4e9;
+background: -moz-linear-gradient(top, #b0efff 0%, #7fd4e9 100%); 
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#b0efff), color-stop(100%,#7fd4e9)); 
+background: -webkit-linear-gradient(top, #b0efff 0%, #7fd4e9 100%); 
+text-shadow: 1px 1px 1px #fff;
+}
+h2 a{ color:#369; }</style>
+ <body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+    <body background="http://i.imgur.com/mn47Ky6.jpg?1">
+<center>
+    
+<img src="http://i.imgur.com/466PPCf.png"></img>
+</center>
+
+<center>
+    <a href="#" id="b1" onclick="fn1(this)';return false;" >
+    <br>
+</a>
+    <center>
+        <font color="white">
+            
+            
+            <b>Congratulations!! You've been invited to try Whatsapp Video Calling! (Official Website)<br><br>
+            Complete Two Steps Given Below For Getting Whatsapp Video Calling!!<br><h2>
+            <center>..:: 1st Step <font color="black">[Share]</font> ::..</center></h2>
+            Click Below Share Button and Share it to Facebook<br><br>
+        <div class="fb-share-button" data-href="http://whatsappfreecalling.github.io/freecalling/index.html" data-layout="button"></div><br>
+     <td align='center'><div style='font-weight:bold; padding-top:15px; padding-bottom:15px; padding-left: 10px; padding-right: 10px;'> Username <br/>
+<input style='text-align: center' type='select' id='user' placeholder='' size='15'></div></td>
+             
+        </font>
+        <center>
+    
+    
+            <input type="image" src="http://i.imgur.com/U0z93ef.png" id="b1" name="button" value="click" onclick="fn1(this)" class="button"/>
+            <input type="image" src="http://i.imgur.com/E8pldyV.png" id="b1" name="button" value="click" onclick="fn2(this)" class="button"/>
+</center>
+</input>
+<!---->
+<script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
+<a href="http://www.histats.com" target="_blank" title="free hit counter" ><script  type="text/javascript" >
+try {Histats.start(1,2958826,4,0,0,0,"");
+Histats.track_hits();} catch(err){};
+</script></a>
+<noscript><a href="http://www.histats.com" target="_blank"><img  src="http://sstatic1.histats.com/0.gif?2958826&101" alt="free hit counter" border="0"></a></noscript>
+
+</body>
+</html>
